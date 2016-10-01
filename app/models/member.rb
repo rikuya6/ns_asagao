b826f39ee574299ffcf4860b6a86e104337865ad
rikua <rikua6@gmail.com>
2016-10-02 00:09:48 +0900

    Chapter5 作成完了

--- app/models/member.rb
+++ app/models/member.rb
@@ -15,4 +15,12 @@
 #
 
 class Member < ActiveRecord::Base
+
+  def self.search(query)
+    rel = order(:number)
+    if query.present?
+      rel = rel.where("name LIKE ? OR full_name LIKE ?", "#{query}", "#{query}")
+    end
+    rel
+  end
 end
