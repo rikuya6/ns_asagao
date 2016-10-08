24a5993c9a85aff46239a8cebd492d6cf353bc72
rikua <rikua6@gmail.com>
2016-10-08 23:15:47 +0900

    Chapter6.3まで終了

--- app/models/member.rb
+++ app/models/member.rb
@@ -15,6 +15,40 @@
 #
 
 class Member < ActiveRecord::Base
+  include EmailAddressChecker
+
+
+  validates :number,      presence: true,
+                          numericality: {
+                                          only_integer: true,
+                                          greater_than: 0,
+                                          less_then: 100,
+                                          allow_blank: true,
+                                        },
+                          uniqueness: true
+
+  validates :name,        presence: true,
+                          format: {
+                                    with: /\A[A-Za-z]\w*\z/, allow_blank: true,
+                                    message: :invalid_member_name,
+                                  },
+                          length: {
+                                    minimum: 2,
+                                    maximum: 20,
+                                    allow_blank: true,
+                                   },
+                          uniqueness: {
+                                        case_sensitive: false,
+                                      }
+
+  validates :full_name,   length: {
+                                    maximum: 20,
+                                  }
+
+  validate  :check_email
+
+
+  private
 
   def self.search(query)
     rel = order(:number)
@@ -22,5 +56,11 @@ class Member < ActiveRecord::Base
       rel = rel.where("name LIKE ? OR full_name LIKE ?", "#{query}", "#{query}")
     end
     rel
+  end
+
+  def check_email
+    if email.present?
+      errors.add(:email, :invalid) unless well_formed_as_email_address(:email)
+    end
   end
 end
