80ec963d55811a35ac693dce7e3155303014c82f
rikua <rikua6@gmail.com>
2016-10-22 22:59:24 +0900

    Production seeds追加

---
+++ db/seeds/production/members.rb
@@ -0,0 +1,28 @@
+Member.create(
+  number: 88,
+  name: 'Rikuya',
+  full_name: 'Rikuya',
+  email: 'test@test.com',
+  birthday: '1981-01-01',
+  gender: 0,
+  administrator: true,
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+names = %w(Taro Jiro Hana John Mike Sophy Bill Alex Mary Tom)
+fnames = %w(佐藤 鈴木 高橋 田中)
+gnames = %w(太郎 二郎 花子)
+0.upto(9) do |idx|
+  Member.create(
+    number: idx + 10,
+    name: names[idx],
+    full_name: "#{fnames[idx % fnames.length]} #{gnames[idx % gnames.length]}",
+    email: "#{names[idx]}@example.com",
+    birthday: '1981-12-01',
+    gender: [0, 0, 0][idx % 3],
+    administrator: idx.zero?,
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
